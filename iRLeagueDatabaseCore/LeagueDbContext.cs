c8cb833e21696251e70bdc743000d1859822d4b7
Simon Schulze <simonschulze2589@hotmail.de>
2022-03-10 22:23:34 +0100

    Remove default optionsbuilder

--- iRLeagueDatabaseCore/LeagueDbContext.cs
+++ iRLeagueDatabaseCore/LeagueDbContext.cs
@@ -47,12 +47,6 @@ namespace iRLeagueDatabaseCore.Models
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-//                optionsBuilder.UseSqlServer("Server=.\\IRLEAGUEDB;Database=LeagueDbCore;Trusted_Connection=True;");
-                  optionsBuilder.UseMySQL("server=localhost;database=TestDatabase;user=simon;password=Cah3x8b3!");
-            }
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
