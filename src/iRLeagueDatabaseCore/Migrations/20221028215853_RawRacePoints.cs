6566ef3bcbef54f4de7944d9602142eb65b6409a
Simon Schulze <simonschulze2589@hotmail.de>
2022-10-28 23:59:09 +0200

    Add raw race points

---
+++ src/iRLeagueDatabaseCore/Migrations/20221028215853_RawRacePoints.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace iRLeagueDatabaseCore.Migrations
+{
+    public partial class RawRacePoints : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "RacePoints",
+                table: "ResultRows",
+                type: "double",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RacePoints",
+                table: "ResultRows");
+        }
+    }
+}
