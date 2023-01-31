163477e9cb7e1fa78ea9593c8bc2df8772b76360
Simon Schulze <simonschulze2589@hotmail.de>
2023-01-31 16:39:09 +0100

    add entities

---
+++ src/iRLeagueDatabaseCore/Models/ChampSeasonEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace iRLeagueDatabaseCore.Models;
+public partial class ChampSeasonEntity
+{
+    public long LeagueId { get; set; }
+    public long ChampionShipId { get; set; }
+    public long SeasonId { get; set; }
+
+    public virtual ICollection<ResultConfigurationEntity> ResultConfigurations { get; set; }
+    public virtual StandingConfigurationEntity StandingConfiguration { get; set; }
+}
---
+++ src/iRLeagueDatabaseCore/Models/ChampionshipEntity.cs
@@ -0,0 +1,35 @@
+﻿namespace iRLeagueDatabaseCore.Models;
+public partial class ChampionshipEntity : IVersionEntity
+{
+    public long LeagueId { get; set; }
+    public long ChampionshipId { get; set; }
+    public string Name { get; set; }
+    public string DisplayName { get; set; }
+
+    #region version
+    public DateTime? CreatedOn { get; set; }
+    public DateTime? LastModifiedOn { get; set; }
+    public int Version { get; set; }
+    public string CreatedByUserId { get; set; }
+    public string CreatedByUserName { get; set; }
+    public string LastModifiedByUserId { get; set; }
+    public string LastModifiedByUserName { get; set; }
+    #endregion
+}
+
+public sealed class ChampionshipEntityConfiguration : IEntityTypeConfiguration<ChampionshipEntity>
+{
+    public void Configure(EntityTypeBuilder<ChampionshipEntity> entity)
+    {
+        entity.HasKey(e => new { e.LeagueId, e.ChampionshipId });
+
+        entity.HasAlternateKey(e => e.ChampionshipId);
+
+        entity.Property(e => e.ChampionshipId)
+            .ValueGeneratedOnAdd();
+
+        entity.Property(e => e.Name).HasMaxLength(80);
+
+        entity.Property(e => e.DisplayName).HasMaxLength(255);
+    }
+}
